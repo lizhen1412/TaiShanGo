4bedf59235eabbb3c2a97ee117dcf7ad11217fc0
Lambert <595265454@qq.com>
2024-02-23 01:08:33 +0800

    修复数据库样例

--- examples/mysql_example.go
+++ examples/mysql_example.go
@@ -8,6 +8,7 @@ import (
 	"github.com/lizhen1412/TaiShanGo/database"
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
+	"gorm.io/gorm/logger"
 )
 
 type User struct {
@@ -35,9 +36,19 @@ func main() {
 	dsn := fmt.Sprintf("%s:%s@tcp(%s:%d)/%s?charset=utf8mb4&parseTime=True&loc=Local",
 		dbConfig.User, dbConfig.Password, dbConfig.Host, dbConfig.Port, dbConfig.Name)
 
-	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{
+		Logger: logger.Default.LogMode(logger.Info),
+		// 使用自定义连接池设置
+		PrepareStmt: true, // 启用预编译语句
+	})
 	if err != nil {
 		panic("failed to connect database")
+	}
+
+	// 执行数据库迁移
+	err = db.AutoMigrate(&User{})
+	if err != nil {
+		panic("failed to migrate database")
 	}
 
 	userRepo := &database.GormRepository[User]{}
